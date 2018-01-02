e9b2b3d54ada82f70cc8304d123f34b1be9d48ea
aleksey-slavik <aleksey.slavik92@gmail.com>
2018-01-02 18:09:42 +0200

    add jersey user rest service

--- src/main/java/com/globallogic/store/rest/jersey/UserController.java
+++ src/main/java/com/globallogic/store/rest/jersey/UserController.java
@@ -3,9 +3,7 @@ package com.globallogic.store.rest.jersey;
 import com.globallogic.store.dao.AbstractGenericDAO;
 import com.globallogic.store.model.User;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -19,5 +17,44 @@ public class UserController {
     @Produces(MediaType.APPLICATION_JSON)
     public List<User> getUserList() {
         return userDao.findAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUserById(@PathParam("id") Long id) {
+        return userDao.findById(id);
+    }
+
+    @GET
+    @Path("/search/{key}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> searchUser(@PathParam("key") String key) {
+        return userDao.findByKey(key, "firstname", "lastname", "username");
+    }
+
+    @POST
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public User createUser(User user) {
+        return userDao.create(user);
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public User updateUser(@PathParam("id") Long id, User user) {
+        user.setId(id);
+        return userDao.update(user);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public User DeleteUser(@PathParam("id") Long id) {
+        return userDao.delete(id);
     }
 }
