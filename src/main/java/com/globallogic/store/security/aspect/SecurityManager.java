e9ded3a3387f31d4ed359b9ea8b4a9e9539093a2
aleksey-slavik <aleksey.slavik92@gmail.com>
2018-01-09 19:03:08 +0200

    add security using aspect for users

--- src/main/java/com/globallogic/store/security/aspect/SecurityManager.java
+++ src/main/java/com/globallogic/store/security/aspect/SecurityManager.java
@@ -1,26 +1,40 @@
 package com.globallogic.store.security.aspect;
 
+import com.globallogic.store.dao.AbstractDAO;
 import com.globallogic.store.model.Role;
+import com.globallogic.store.model.User;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.servlet.ModelAndView;
 
-@Aspect
+import java.util.HashMap;
+
 public class SecurityManager {
 
-    @Around("execution(* com.globallogic.store.web.user.ShowUser.handleRequest(..)) && @target(access)")
-    public ModelAndView checkAccess(ProceedingJoinPoint joinPoint, Access access) {
+    private AbstractDAO<User> userDAO;
 
+    public void setUserDAO(AbstractDAO<User> userDAO) {
+        this.userDAO = userDAO;
+    }
 
-        if (access.role().equals(Role.ADMIN)) {
-            try {
-                return (ModelAndView) joinPoint.proceed();
-            } catch (Throwable throwable) {
-                return new ModelAndView("error/error");
+    public ModelAndView checkAccess(ProceedingJoinPoint joinPoint) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        System.out.println(username);
+
+        if (username != null) {
+            HashMap<String, String> params = new HashMap<String, String>();
+            params.put("username", username);
+            User user = userDAO.findByCriteria(params).get(0);
+
+            if (user.getRole() == Role.ADMIN) {
+                try {
+                    joinPoint.proceed();
+                } catch (Throwable throwable) {
+                    return new ModelAndView("error/403");
+                }
             }
-        } else {
-            return new ModelAndView("error/403");
         }
+
+        return new ModelAndView("error/403");
     }
 }
