788948b7750b122b8bb91360fd18ae993c64bffe
aleksey-slavik <aleksey.slavik92@gmail.com>
2018-01-12 18:42:00 +0200

    add aspect based security manager

--- src/main/java/com/globallogic/store/security/aspect/SecurityManager.java
+++ src/main/java/com/globallogic/store/security/aspect/SecurityManager.java
@@ -1,35 +1,32 @@
 package com.globallogic.store.security.aspect;
 
-import com.globallogic.store.dao.AbstractDAO;
-import com.globallogic.store.model.Role;
-import com.globallogic.store.model.User;
+import com.globallogic.store.exception.AccessDeniedException;
+import com.globallogic.store.security.spring.Authenticateble;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.HashMap;
 
 public class SecurityManager {
 
-    public ModelAndView checkAccess(ProceedingJoinPoint joinPoint) throws Throwable {
+    private Authenticateble authenticationFacade;
+
+    public void setAuthenticationFacade(Authenticateble authenticationFacade) {
+        this.authenticationFacade = authenticationFacade;
+    }
+
+    public Object checkAccess(ProceedingJoinPoint joinPoint) throws Throwable {
+        Authentication authentication = authenticationFacade.getAuthentication();
+        System.out.println("SECURITY: " + SecurityContextHolder.getContext().getAuthentication());
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        System.out.println(principal.toString());
-
         if (principal instanceof UserDetails) {
-            System.out.println(((UserDetails) principal).getUsername());
-            System.out.println(((UserDetails) principal).getAuthorities());
-
             if (((UserDetails) principal).getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"))) {
-
-                return (ModelAndView) joinPoint.proceed();
+                return joinPoint.proceed();
             }
         }
 
-        return new ModelAndView("error/403");
+        throw new AccessDeniedException();
     }
 }
