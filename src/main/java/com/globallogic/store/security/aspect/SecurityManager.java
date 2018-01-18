9d73b87e5ad1a17f0aac99e1b9ce3922cecededb
aleksey-slavik <aleksey.slavik92@gmail.com>
2018-01-18 13:05:02 +0200

    add customer access security

--- src/main/java/com/globallogic/store/security/aspect/SecurityManager.java
+++ src/main/java/com/globallogic/store/security/aspect/SecurityManager.java
@@ -1,6 +1,8 @@
 package com.globallogic.store.security.aspect;
 
 import com.globallogic.store.exception.AccessDeniedException;
+import com.globallogic.store.model.User;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,12 +15,38 @@ public class SecurityManager {
         this.authenticationFacade = authenticationFacade;
     }
 
-    public Object checkAccess(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object checkAdminAccess(ProceedingJoinPoint joinPoint) throws Throwable {
         Object principal = authenticationFacade.getAuthentication().getPrincipal();
 
         if (principal instanceof UserDetails) {
             if (((UserDetails) principal).getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"))) {
                 return joinPoint.proceed();
+            }
+        }
+
+        throw new AccessDeniedException();
+    }
+
+    private User user;
+
+    public void getAuthorityBefore(JoinPoint joinPoint) {
+        System.out.println("logBefore() is running!");
+        System.out.println("args length : " + joinPoint.getArgs().length);
+        System.out.println("id : " + joinPoint.getArgs()[0]);
+        System.out.println("user : " + joinPoint.getArgs()[1]);
+        user = (User) joinPoint.getArgs()[1];
+    }
+
+    public Object checkAuthorizedAccess(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object principal = authenticationFacade.getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            if (((UserDetails) principal).getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"))) {
+                return joinPoint.proceed();
+            } else if (((UserDetails) principal).getAuthorities().contains(new SimpleGrantedAuthority("CUSTOMER"))) {
+                if (((UserDetails) principal).getUsername().equals(user.getUsername())) {
+                    return joinPoint.proceed();
+                }
             }
         }
 
