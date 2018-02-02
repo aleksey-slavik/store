ea0798b4cc7ec1e44e36c196e5c5925472b2fda4
aleksey-slavik <aleksey.slavik92@gmail.com>
2018-02-02 18:06:33 +0200

    update sql dump

--- sql/store.sql
+++ sql/store.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 9.6.6
 -- Dumped by pg_dump version 9.6.6
 
--- Started on 2018-01-25 19:04:30
+-- Started on 2018-02-02 18:06:04
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -18,7 +18,7 @@ SET row_security = off;
 
 DROP DATABASE store;
 --
--- TOC entry 2173 (class 1262 OID 16384)
+-- TOC entry 2177 (class 1262 OID 16384)
 -- Name: store; Type: DATABASE; Schema: -; Owner: root
 --
 
@@ -47,7 +47,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2176 (class 0 OID 0)
+-- TOC entry 2180 (class 0 OID 0)
 -- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -92,7 +92,7 @@ CREATE SEQUENCE order_id_seq
 ALTER TABLE order_id_seq OWNER TO root;
 
 --
--- TOC entry 2177 (class 0 OID 0)
+-- TOC entry 2181 (class 0 OID 0)
 -- Dependencies: 190
 -- Name: order_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: root
 --
@@ -110,7 +110,7 @@ CREATE TABLE order_item (
     quantity integer DEFAULT 0 NOT NULL,
     price real NOT NULL,
     id bigint NOT NULL,
-    order_id bigint NOT NULL
+    order_id bigint DEFAULT 1
 );
 
 
@@ -132,7 +132,7 @@ CREATE SEQUENCE order_item_id_seq
 ALTER TABLE order_item_id_seq OWNER TO root;
 
 --
--- TOC entry 2178 (class 0 OID 0)
+-- TOC entry 2182 (class 0 OID 0)
 -- Dependencies: 192
 -- Name: order_item_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: root
 --
@@ -172,7 +172,7 @@ CREATE SEQUENCE product_id_seq
 ALTER TABLE product_id_seq OWNER TO root;
 
 --
--- TOC entry 2179 (class 0 OID 0)
+-- TOC entry 2183 (class 0 OID 0)
 -- Dependencies: 186
 -- Name: product_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: root
 --
@@ -214,7 +214,7 @@ CREATE SEQUENCE user_id_seq
 ALTER TABLE user_id_seq OWNER TO root;
 
 --
--- TOC entry 2180 (class 0 OID 0)
+-- TOC entry 2184 (class 0 OID 0)
 -- Dependencies: 188
 -- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: root
 --
@@ -255,58 +255,57 @@ ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('user_id_seq'::regcla
 
 
 --
--- TOC entry 2181 (class 0 OID 0)
+-- TOC entry 2185 (class 0 OID 0)
 -- Dependencies: 190
 -- Name: order_id_seq; Type: SEQUENCE SET; Schema: public; Owner: root
 --
 
-SELECT pg_catalog.setval('order_id_seq', 22, true);
+SELECT pg_catalog.setval('order_id_seq', 31, true);
 
 
 --
--- TOC entry 2167 (class 0 OID 24655)
+-- TOC entry 2171 (class 0 OID 24655)
 -- Dependencies: 191
 -- Data for Name: order_item; Type: TABLE DATA; Schema: public; Owner: root
 --
 
-INSERT INTO order_item (product_id, quantity, price, id, order_id) VALUES (4, 7, 666, 27, 1);
-INSERT INTO order_item (product_id, quantity, price, id, order_id) VALUES (4, 3, 467, 7, 5);
-INSERT INTO order_item (product_id, quantity, price, id, order_id) VALUES (5, 1, 398745, 12, 5);
-INSERT INTO order_item (product_id, quantity, price, id, order_id) VALUES (36, 12, 777, 28, 5);
+INSERT INTO order_item (product_id, quantity, price, id, order_id) VALUES (4, 3, 467, 7, 1);
+INSERT INTO order_item (product_id, quantity, price, id, order_id) VALUES (36, 2, 36563, 54, 5);
 
 
 --
--- TOC entry 2182 (class 0 OID 0)
+-- TOC entry 2186 (class 0 OID 0)
 -- Dependencies: 192
 -- Name: order_item_id_seq; Type: SEQUENCE SET; Schema: public; Owner: root
 --
 
-SELECT pg_catalog.setval('order_item_id_seq', 28, true);
+SELECT pg_catalog.setval('order_item_id_seq', 54, true);
 
 
 --
--- TOC entry 2165 (class 0 OID 24626)
+-- TOC entry 2169 (class 0 OID 24626)
 -- Dependencies: 189
 -- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: root
 --
 
 INSERT INTO orders (id, total_cost, user_id, status) VALUES (1, 1111, 1, 'OPENED');
-INSERT INTO orders (id, total_cost, user_id, status) VALUES (5, 86876, 36, 'OPENED');
+INSERT INTO orders (id, total_cost, user_id, status) VALUES (31, 0, 7, 'OPENED');
+INSERT INTO orders (id, total_cost, user_id, status) VALUES (5, 73126, 36, 'OPENED');
 
 
 --
--- TOC entry 2161 (class 0 OID 16385)
+-- TOC entry 2165 (class 0 OID 16385)
 -- Dependencies: 185
 -- Data for Name: product; Type: TABLE DATA; Schema: public; Owner: root
 --
 
 INSERT INTO product (name, brand, description, price, id) VALUES ('iPhone X', 'Apple', 'Display (5.8", OLED (Super Retina HD), 2436x1125)/Apple A11 Bionic/ RAM 3 ГБ/ 3G/ LTE/ GPS/ iOS 11', 44999, 4);
-INSERT INTO product (name, brand, description, price, id) VALUES ('Redmi 4X 4/64GB', 'Xiaomi', 'Display (IPS, 1280x720)/ Qualcomm Snapdragon 435 (1.4 ГГц)/ 3G/ LTE/ GPS/ GLONASS/ Android 6.0 (Marshmallow) / 4100 mA*h', 4999, 5);
 INSERT INTO product (name, brand, description, price, id) VALUES ('Galaxy S8 64GB', 'Samsung', 'Display (5.8", Super AMOLED, 2960х1440)/ Samsung Exynos 8895 (4 x 2.3 ГГц + 4 x 1.7 ГГц)/ 3G/ LTE/ GPS/  Android 7.0 (Nougat) / 3000 мА*h', 22999, 36);
+INSERT INTO product (name, brand, description, price, id) VALUES ('Redmi 4X 4/64GB', 'Xiaomi', 'Display (IPS, 1280x720)/ Qualcomm Snapdragon 435 (1.4 ГГц)/ 3G/ LTE/ GPS/ GLONASS/ Android 6.0 (Marshmallow) / 4100 mA*h', 4999, 5);
 
 
 --
--- TOC entry 2183 (class 0 OID 0)
+-- TOC entry 2187 (class 0 OID 0)
 -- Dependencies: 186
 -- Name: product_id_seq; Type: SEQUENCE SET; Schema: public; Owner: root
 --
@@ -315,7 +314,7 @@ SELECT pg_catalog.setval('product_id_seq', 40, true);
 
 
 --
--- TOC entry 2184 (class 0 OID 0)
+-- TOC entry 2188 (class 0 OID 0)
 -- Dependencies: 188
 -- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: root
 --
@@ -324,7 +323,7 @@ SELECT pg_catalog.setval('user_id_seq', 62, true);
 
 
 --
--- TOC entry 2163 (class 0 OID 16407)
+-- TOC entry 2167 (class 0 OID 16407)
 -- Dependencies: 187
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: root
 --
@@ -336,7 +335,7 @@ INSERT INTO users (id, firstname, lastname, username, password, email, role) VAL
 
 
 --
--- TOC entry 2039 (class 2606 OID 24736)
+-- TOC entry 2040 (class 2606 OID 24736)
 -- Name: order_item order_item_id_pk; Type: CONSTRAINT; Schema: public; Owner: root
 --
 
@@ -345,7 +344,7 @@ ALTER TABLE ONLY order_item
 
 
 --
--- TOC entry 2036 (class 2606 OID 24633)
+-- TOC entry 2037 (class 2606 OID 24633)
 -- Name: orders order_pkey; Type: CONSTRAINT; Schema: public; Owner: root
 --
 
@@ -354,7 +353,7 @@ ALTER TABLE ONLY orders
 
 
 --
--- TOC entry 2029 (class 2606 OID 16399)
+-- TOC entry 2030 (class 2606 OID 16399)
 -- Name: product product_pkey; Type: CONSTRAINT; Schema: public; Owner: root
 --
 
@@ -363,7 +362,7 @@ ALTER TABLE ONLY product
 
 
 --
--- TOC entry 2031 (class 2606 OID 16414)
+-- TOC entry 2032 (class 2606 OID 16414)
 -- Name: users user_pkey; Type: CONSTRAINT; Schema: public; Owner: root
 --
 
@@ -372,7 +371,7 @@ ALTER TABLE ONLY users
 
 
 --
--- TOC entry 2034 (class 1259 OID 24654)
+-- TOC entry 2035 (class 1259 OID 24654)
 -- Name: fki_order_user; Type: INDEX; Schema: public; Owner: root
 --
 
@@ -380,7 +379,7 @@ CREATE INDEX fki_order_user ON orders USING btree (user_id);
 
 
 --
--- TOC entry 2037 (class 1259 OID 24663)
+-- TOC entry 2038 (class 1259 OID 24663)
 -- Name: fki_product; Type: INDEX; Schema: public; Owner: root
 --
 
@@ -388,7 +387,7 @@ CREATE INDEX fki_product ON order_item USING btree (product_id);
 
 
 --
--- TOC entry 2040 (class 1259 OID 24734)
+-- TOC entry 2041 (class 1259 OID 24734)
 -- Name: order_item_id_uindex; Type: INDEX; Schema: public; Owner: root
 --
 
@@ -396,7 +395,7 @@ CREATE UNIQUE INDEX order_item_id_uindex ON order_item USING btree (id);
 
 
 --
--- TOC entry 2032 (class 1259 OID 24818)
+-- TOC entry 2033 (class 1259 OID 24818)
 -- Name: users_email_uindex; Type: INDEX; Schema: public; Owner: root
 --
 
@@ -404,7 +403,7 @@ CREATE UNIQUE INDEX users_email_uindex ON users USING btree (email);
 
 
 --
--- TOC entry 2033 (class 1259 OID 24837)
+-- TOC entry 2034 (class 1259 OID 24837)
 -- Name: users_username_uindex; Type: INDEX; Schema: public; Owner: root
 --
 
@@ -412,7 +411,25 @@ CREATE UNIQUE INDEX users_username_uindex ON users USING btree (username);
 
 
 --
--- TOC entry 2041 (class 2606 OID 24649)
+-- TOC entry 2043 (class 2606 OID 25011)
+-- Name: orders fk32ql8ubntj5uh44ph9659tiih; Type: FK CONSTRAINT; Schema: public; Owner: root
+--
+
+ALTER TABLE ONLY orders
+    ADD CONSTRAINT fk32ql8ubntj5uh44ph9659tiih FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+--
+-- TOC entry 2047 (class 2606 OID 25006)
+-- Name: order_item fk551losx9j75ss5d6bfsqvijna; Type: FK CONSTRAINT; Schema: public; Owner: root
+--
+
+ALTER TABLE ONLY order_item
+    ADD CONSTRAINT fk551losx9j75ss5d6bfsqvijna FOREIGN KEY (product_id) REFERENCES product(id);
+
+
+--
+-- TOC entry 2042 (class 2606 OID 24649)
 -- Name: orders fk_order_user; Type: FK CONSTRAINT; Schema: public; Owner: root
 --
 
@@ -421,7 +438,7 @@ ALTER TABLE ONLY orders
 
 
 --
--- TOC entry 2042 (class 2606 OID 24658)
+-- TOC entry 2044 (class 2606 OID 24658)
 -- Name: order_item fk_product; Type: FK CONSTRAINT; Schema: public; Owner: root
 --
 
@@ -430,7 +447,16 @@ ALTER TABLE ONLY order_item
 
 
 --
--- TOC entry 2043 (class 2606 OID 24940)
+-- TOC entry 2046 (class 2606 OID 25001)
+-- Name: order_item fkt4dc2r9nbvbujrljv3e23iibt; Type: FK CONSTRAINT; Schema: public; Owner: root
+--
+
+ALTER TABLE ONLY order_item
+    ADD CONSTRAINT fkt4dc2r9nbvbujrljv3e23iibt FOREIGN KEY (order_id) REFERENCES orders(id);
+
+
+--
+-- TOC entry 2045 (class 2606 OID 24996)
 -- Name: order_item order_item_orders_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: root
 --
 
@@ -439,7 +465,7 @@ ALTER TABLE ONLY order_item
 
 
 --
--- TOC entry 2175 (class 0 OID 0)
+-- TOC entry 2179 (class 0 OID 0)
 -- Dependencies: 6
 -- Name: public; Type: ACL; Schema: -; Owner: root
 --
@@ -447,7 +473,7 @@ ALTER TABLE ONLY order_item
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2018-01-25 19:04:31
+-- Completed on 2018-02-02 18:06:04
 
 --
 -- PostgreSQL database dump complete
