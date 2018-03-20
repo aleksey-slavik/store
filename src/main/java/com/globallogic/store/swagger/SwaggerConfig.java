ed9221d41cbb8d49a9c25a0cacf33362a10cc43e
aleksey-slavik <aleksey.slavik92@gmail.com>
2018-03-20 15:34:00 +0200

    javadoc review

--- src/main/java/com/globallogic/store/swagger/SwaggerConfig.java
+++ src/main/java/com/globallogic/store/swagger/SwaggerConfig.java
@@ -1,9 +1,32 @@
 package com.globallogic.store.swagger;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * Swagger configuration
+ *
+ * @author oleksii.slavik
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    /**
+     * Setup swagger configuration only for rest service
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.basePackage("com.globallogic.store.rest"))
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/api");
+
+    }
 }
